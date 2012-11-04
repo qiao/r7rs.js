8e668b3f7014b3d1f430a96336212b133f20ff5a
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-11-04 13:42:05 +0800

    add: test of scan for boolean

--- test/lexer.test.js
+++ test/lexer.test.js
@@ -83,9 +83,13 @@ describe('Lexer', function () {
           'a34kTMNs',
           '...'].forEach(function (id) {
               lexer = new Lexer(id);
-              lexer.scanIdentifier().should.eql({
+              lexer.nextToken().should.eql({
                   type: 'identifier',
                   value: id,
+                  lineNumber: 1
+              });
+              lexer.nextToken().should.eql({
+                  type: 'EOF',
                   lineNumber: 1
               });
           });
@@ -94,11 +98,63 @@ describe('Lexer', function () {
            '|two words|',
            '|two\\x20;words|'].forEach(function (id) {
               lexer = new Lexer(id);
-              lexer.scanIdentifier().should.eql({
+              lexer.nextToken().should.eql({
                   type: 'identifier',
                   value: 'two words',
                   lineNumber: 1
               });
+              lexer.nextToken().should.eql({
+                  type: 'EOF',
+                  lineNumber: 1
+              });
           });
+
+          lexer = new Lexer(';\ntwo words;\r\n');
+          lexer.nextToken().should.eql({
+              type: 'identifier',
+              value: 'two',
+              lineNumber: 2
+          });
+          lexer.nextToken().should.eql({
+              type: 'identifier',
+              value: 'words',
+              lineNumber: 2
+          });
+          lexer.nextToken().should.eql({
+              type: 'EOF',
+              lineNumber: 3
+          });
+    });
+
+    it('should scan boolean', function () {
+        lexer = new Lexer('#t');
+        lexer.nextToken().should.eql({
+            type: 'boolean',
+            value: true,
+            lineNumber: 1
+        });
+
+        lexer = new Lexer('#false');
+        lexer.nextToken().should.eql({
+            type: 'boolean',
+            value: false,
+            lineNumber: 1
+        });
+
+        lexer = new Lexer('#false  ;\n#true\r\n');
+        lexer.nextToken().should.eql({
+            type: 'boolean',
+            value: false,
+            lineNumber: 1
+        });
+        lexer.nextToken().should.eql({
+            type: 'boolean',
+            value: true,
+            lineNumber: 2
+        });
+        lexer.nextToken().should.eql({
+            type: 'EOF',
+            lineNumber: 3
+        });
     });
 });
