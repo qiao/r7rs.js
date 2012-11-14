01976632a8335b7b8e9950c540ee381ca4f19125
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-11-15 01:52:40 +0800

    Make sure that there is only one copy for each symbol

--- src/objects/symbol.js
+++ src/objects/symbol.js
@@ -1,6 +1,14 @@
 function Symbol(name) {
-    this.name = name;
+    // make sure that there is exactly one copy for each symbol
+    if (Object.hasOwnProperty.call(Symbol.symbols, name)) {
+        return Symbol.symbols[name];
+    } else {
+        this.name = name;
+        Symbol.symbols[name] = this;
+    }
 }
+
+Symbol.symbols = {}; // allocated symbols
 
 Symbol.prototype.type = 'symbol';
 
