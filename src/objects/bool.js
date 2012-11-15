267981a7613c94d6c02fd91b35d9dda796856621
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-11-15 20:20:57 +0800

    Make sure `true` and `false` are unique

--- src/objects/bool.js
+++ src/objects/bool.js
@@ -1,6 +1,17 @@
 function Bool(value) {
     this.value = value;
+
+    // if Bool.True and Bool.False are already defined,
+    // then return the corresponding instance.
+    if (this.value && Bool.True) {
+        return Bool.True;
+    } else if (!this.value && Bool.False) {
+        return Bool.False;
+    }
 }
+
+Bool.True = new Bool(true);
+Bool.False = new Bool(false);
 
 Bool.prototype.type = 'bool';
 
