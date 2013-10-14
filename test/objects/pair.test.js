145e9bc89a709d5f0173f6b34fd66a4b0136bde6
Xueqiao Xu <xueqiaoxu@gmail.com>
2013-10-13 22:17:00 -0400

    add: test for `Pair#isProperList`

--- test/objects/pair.test.js
+++ test/objects/pair.test.js
@@ -10,7 +10,7 @@ describe('Pair', function () {
 
     var pair;
 
-    describe('.getLength()', function () {
+    describe('#getLength()', function () {
         it('should return the length when it\'s a proper list', function () {
             pair = new Pair(new Real(1), Nil);
             pair.getLength().should.equal(1);
@@ -32,7 +32,19 @@ describe('Pair', function () {
         });
     });
 
-    describe('.isProperList()', function () {
+    describe('#reverse()', function () {
+        it('should return the reversed list', function () {
+            pair = new Pair(new Real(1), Nil);
+            pair.reverse().should.eql(new Pair(new Real(1), Nil));
+
+            pair = new Pair(new Real(1), new Pair(new Real(2), Nil));
+            pair.reverse().should.eql(
+                new Pair(new Real(2), new Pair(new Real(1), Nil))
+            );
+        });
+    });
+
+    describe('#isProperList()', function () {
         it('should return true when it\'s a proper list', function () {
             pair = new Pair(new Real(1), Nil);
             pair.isProperList().should.be.true;
@@ -50,15 +62,26 @@ describe('Pair', function () {
         });
     });
 
-    describe('.reverse()', function () {
-        it('should return the reversed list', function () {
-            pair = new Pair(new Real(1), Nil);
-            pair.reverse().should.eql(new Pair(new Real(1), Nil));
-
+    describe('#toProperList()', function () {
+        it('should return the identical list if it\'s already proper', function () {
             pair = new Pair(new Real(1), new Pair(new Real(2), Nil));
-            pair.reverse().should.eql(
-                new Pair(new Real(2), new Pair(new Real(1), Nil))
-            );
+            pair.toProperList().toJSON().should.eql({
+                type: 'pair',
+                car: {
+                    type: 'real',
+                    value: 1
+                },
+                cdr: {
+                    type: 'pair',
+                    car: {
+                        type: 'real',
+                        value: 2
+                    },
+                    cdr: {
+                        type: 'nil'
+                    }
+                }
+            });
         });
     });
 });
