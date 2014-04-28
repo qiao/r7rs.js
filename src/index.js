2642cccde5b3c8583d4a24dd8007bddee8450b35
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-04-27 20:42:48 -0700

    add: facade function to evaluate string

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+var parse = require('./parser').parse,
+    compile = require('./compiler').compile,
+    execute = require('./interpreter').execute,
+    objects = require('./objects');
+
+
+function evaluate(source) {
+    return execute(compile(parse(source)[0]));
+}
+
 module.exports = {
-    parse: require('./parser').parse,
-    compile: require('./compiler').compile,
-    execute: require('./interpreter').execute,
-    objects: require('./objects')
+    parse: parse,
+    compile: compile,
+    execute: execute,
+    objects: objects,
+    evaluate: evaluate
 };
