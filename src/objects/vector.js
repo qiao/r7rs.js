ad79007021be57976c8a68b309ccab4af30ad7e6
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-05-15 17:57:46 -0700

    add: vector constructors

--- src/objects/vector.js
+++ src/objects/vector.js
@@ -2,6 +2,20 @@ function Vector(elements) {
     this.elements = elements;
 }
 
+Vector.make = function (n) {
+    return new Vector(new Array(n));
+};
+
+Vector.makeWithFill = function (n, fill) {
+    var i, elements = new Array(n);
+
+    for (i = 0; i < n; ++i) {
+        elements[i] = fill;
+    }
+
+    return new Vector(elements);
+};
+
 Vector.prototype.type = 'vector';
 
 Vector.prototype.toJSON = function () {
