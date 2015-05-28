a4ad0e13fe51e512316541ea4f588f2ca48ca41b
Xueqiao Xu <xueqiaoxu@gmail.com>
2015-05-27 23:41:01 -0700

    Rewrite Real in ts.

---
+++ src/objects/real.ts
@@ -0,0 +1,74 @@
+import ScmObject from './scmobject';
+import Type from './type';
+import Bool from './bool';
+
+export default class Real implements ScmObject {
+
+  type: Type = Type.Real;
+
+  value: number;
+
+  constructor(value: number) {
+    this.value = value;
+  }
+
+  toJSON(): Object {
+    return {
+      type: this.type,
+      value: this.value
+    };
+  }
+
+  add(other: Real): Real {
+    return new Real(this.value + other.value);
+  }
+
+  sub(other: Real): Real {
+    return new Real(this.value - other.value);
+  }
+
+  mul(other: Real): Real {
+    return new Real(this.value * other.value);
+  };
+
+  div(other: Real): Real {
+    return new Real(this.value / other.value);
+  };
+
+  neg(other: Real): Real {
+    return new Real(-this.value);
+  }
+
+  eql(other: Real): Bool {
+    return new Bool(this.value === other.value);
+  }
+
+  lt(other: Real): Bool {
+    return new Bool(this.value < other.value);
+  };
+
+  le(other: Real): Bool {
+    return new Bool(this.value <= other.value);
+  };
+
+  gt(other: Real): Bool {
+    return new Bool(this.value > other.value);
+  };
+
+  ge(other: Real): Bool {
+    return new Bool(this.value >= other.value);
+  };
+
+  display(): string {
+    if (this.value === Infinity) {
+      return '+inf.0';
+    }
+    if (this.value === -Infinity) {
+      return '-inf.0';
+    }
+    if (isNaN(this.value)) {
+      return '+nan.0';
+    }
+    return String(this.value);
+  }
+}
