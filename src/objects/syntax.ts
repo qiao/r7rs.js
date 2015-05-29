cc3675f2eaae0fd6994df6401a63f3853bec4680
Xueqiao Xu <xueqiaoxu@gmail.com>
2015-05-28 18:40:40 -0700

    Rewrite Syntax in ts.

---
+++ src/objects/syntax.ts
@@ -0,0 +1,32 @@
+import ScmObject from './scmobject';
+import Type from './type';
+
+export default class Syntax implements ScmObject {
+
+  static syntaxes: { [name: string]: Syntax; } = {};
+
+  type: Type = Type.SYNTAX;
+
+  name: string;
+
+  constructor(name: string) {
+    // make sure that there is exactly one copy for each syntax
+    if (Object.prototype.hasOwnProperty.call(Syntax.syntaxes, name)) {
+      return Syntax.syntaxes[name];
+    } else {
+      this.name = name;
+      Syntax.syntaxes[name] = this;
+    }
+  }
+
+  toJSON() {
+    return {
+      type: this.type,
+      name: this.name
+    };
+  }
+
+  display() {
+    return '#<syntax ' + this.name + '>';
+  }
+}
