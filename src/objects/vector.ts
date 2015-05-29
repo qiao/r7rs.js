8647d8311cb1d21dd72357374a63467e3e81a84f
Xueqiao Xu <xueqiaoxu@gmail.com>
2015-05-28 18:49:19 -0700

    Rewrite Vector in ts.

---
+++ src/objects/vector.ts
@@ -0,0 +1,53 @@
+import ScmObject from './scmobject';
+import Type from './type';
+
+export default class Vector implements ScmObject {
+
+  static make(n: number): Vector {
+    return new Vector(new Array(n));
+  }
+
+  static makeWithFill(n: number, fill: ScmObject): Vector {
+    var elements: Array<ScmObject> = new Array(n);
+
+    for (var i = 0; i < n; ++i) {
+      elements[i] = fill;
+    }
+
+    return new Vector(elements);
+  }
+  
+  type: Type = Type.VECTOR;
+
+  elements: Array<ScmObject>;
+
+  constructor(elements: Array<ScmObject>) {
+    this.elements = elements;
+  }
+
+  getLength(): number {
+    return this.elements.length;
+  }
+
+  ref(i: number): ScmObject {
+    // TODO: throw error on invalid index
+    return this.elements[i];
+  }
+
+  set(i: number, v: ScmObject) {
+    // TODO: throw error on invalid index
+    this.elements[i] = v;
+  }
+
+  toJSON(): Object {
+    return {
+      type: this.type,
+      elements: this.elements
+    };
+  }
+
+  display(): string {
+    // TODO
+    return '\'#()';
+  }
+}
